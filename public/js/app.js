17a2b34eac4f9d9f4f594c16c5e117a50717be50
Lucas Diniz <lucas.mdiniz@live.com>
2020-02-29 15:07:09 -0300

    init commit[

---
+++ public/js/app.js
@@ -0,0 +1,30 @@
+const weatherForm = document.querySelector('form');
+const addressInput = document.querySelector('#address');
+const locationUi = document.querySelector('#location');
+const forecastUi = document.querySelector('#forecast');
+const fetchForecastErrorUi = document.querySelector('#fetchForecastError');
+const loadingUi = document.querySelector('#loading');
+
+weatherForm.addEventListener('submit', e => {
+  e.preventDefault();
+
+  loadingUi.innerText = 'Loading...';
+  locationUi.innerText = '';
+  forecastUi.innerText = '';
+  fetchForecastErrorUi.innerText = '';
+
+  fetch(`http://localhost:5000/weather?address=${addressInput.value}`).then(
+    response => {
+      response.json().then(data => {
+        if (data.error) {
+          fetchForecastErrorUi.innerText = data.error;
+        } else {
+          locationUi.innerText = data.location;
+          forecastUi.innerText = data.forecast;
+        }
+
+        loadingUi.innerText = '';
+      });
+    }
+  );
+});
---
+++ src/utils/forecast.js
@@ -0,0 +1,25 @@
+const request = require('request');
+
+const forecast = (latitude, longitude, callback) => {
+  const url = `https://api.darksky.net/forecast/f037a6fb4206f65c6606dc26a15e4595/${latitude},${longitude}?units=si`;
+
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback('Unable to connect to weather service!', undefined);
+    } else if (body.error) {
+      callback('Unable to find location', undefined);
+    } else {
+      callback(
+        undefined,
+        body.daily.data[0].summary +
+          ' It is currently ' +
+          body.currently.temperature +
+          ' out. There is a ' +
+          body.currently.precipProbability +
+          '% chance of rain today.'
+      );
+    }
+  });
+};
+
+module.exports = forecast;
